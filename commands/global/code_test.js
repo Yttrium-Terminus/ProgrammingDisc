ca655ed048f2f1f2a03a53675a613f7fc988b8f4
exoad <error666jm@gmail.com>
2021-08-05 10:16:13 -0400

    f

--- commands/global/code_test.js
+++ commands/global/code_test.js
@@ -11,9 +11,9 @@ module.exports = {
   run: async (bot, message, args) => {
     try {
       let t = message.content.split(" ").slice(1);
-      let p = await tio(toString(t), 'python3');
+      let p = await tio(t, 'python3');
       message.channel.send("Cont : " + t.join(" ")).cleanContent;
-      message.channel.send("Cont2: " + JSON.stringify(p));
+      message.channel.send("Cont2: " + p.output);
     } catch (e) {
       console.log(e);
     }
