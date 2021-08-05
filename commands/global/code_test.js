ba6411343443eff676e955724a8edbb963a52e9a
exoad <error666jm@gmail.com>
2021-08-05 10:11:52 -0400

    python3

--- commands/global/code_test.js
+++ commands/global/code_test.js
@@ -1,4 +1,5 @@
 var JSCPP = require("JSCPP");
+var tio = require('tio.js');
 
 module.exports = {
   config: {
@@ -10,7 +11,9 @@ module.exports = {
   run: async (bot, message, args) => {
     try {
       let t = message.content.split(" ").slice(1);
-      message.channel.send(t.join(" ")).cleanContent;
+      let p = await tio(t, 'python3');
+      message.channel.send("Cont : " + t.join(" ")).cleanContent;
+      message.channel.send(p);
     } catch (e) {
       console.log(e);
     }
