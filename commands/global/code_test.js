27bc94ba47239dc5aab8d67908da0e5e65e57d7e
exoad <error666jm@gmail.com>
2021-08-05 15:10:04 -0400

    readd

--- commands/global/code_test.js
+++ commands/global/code_test.js
@@ -1,5 +1,5 @@
 var JSCPP = require("JSCPP");
-var tio = require('tio.js');
+var tio = require("tio.js");
 
 module.exports = {
   config: {
@@ -10,12 +10,43 @@ module.exports = {
   },
   run: async (bot, message, args) => {
     try {
-      let t = message.content.split(" ").slice(1);
-      let p = await tio(toString(t), 'python3');
-      message.channel.send("Cont : " + t.join(" ")).cleanContent;
-      message.channel.send("Cont2: " + p.output);
-    } catch (e) {
-      console.log(e);
+      var code = message.content.split(" ").slice(1);
+      JSCPP.run(code.join(" "));
+    } catch (error) {
+      if (error) {
+        message.channel.send("Check your code.");
+      } else {
+        message.channel.send(
+          "Enter your input within the next 10 seconds (Type `ex!` to exit): "
+        );
+        message.channel
+          .awaitMessages((m) => m.author.id == message.author.id, {
+            max: 1,
+            time: 10000,
+          })
+          .then((collected) => {
+            if (collected.first().content.toLowerCase() == "ex!")
+              message.channel.send(
+                "Operation Timeout by `" + message.author.username + "`"
+              );
+            else
+              message.channel.send(
+                JSCPP.run(code.join(" "), collected, config) + " " + output
+              );
+          })
+          .catch(() => {
+            message.reply("No answer after 10 seconds, operation canceled.");
+          });
+        var output = "";
+        var config = {
+          stdio: {
+            write: function (s) {
+              output += s;
+            },
+          },
+          unsigned_overflow: "error", // can be "error"(default), "warn" or "ignore"
+        };
+      }
     }
   },
 };
