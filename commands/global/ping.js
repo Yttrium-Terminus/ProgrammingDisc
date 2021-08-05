465685d29528e4158ff5c627e03c57a9495ff5d8
exoad <error666jm@gmail.com>
2021-08-05 09:36:17 -0400

    add ping command

---
+++ commands/global/ping.js
@@ -0,0 +1,16 @@
+module.exports = {
+  config: {
+    name: `ping`,
+    category: "",
+    description: "",
+    aliases: [``],
+  },
+  run: async (bot, message, args) => {
+    try {
+      message.channel.send("Latency: " + (Date.now() - message.createdTimestamp) + "ms");
+      message.channel.send("API Latency: " + Math.round(bot.ws.ping) + "ms");
+    } catch (e) {
+      console.log(e);
+    }
+  },
+};
