5cffa53d20bcaeaac28ad36e263197cfbb7986d0
exoad <error666jm@gmail.com>
2021-08-05 15:11:15 -0400

    refix

--- commands/global/code_test.js
+++ commands/global/code_test.js
@@ -9,44 +9,36 @@ module.exports = {
     aliases: [``],
   },
   run: async (bot, message, args) => {
-    try {
-      var code = message.content.split(" ").slice(1);
-      JSCPP.run(code.join(" "));
-    } catch (error) {
-      if (error) {
-        message.channel.send("Check your code.");
-      } else {
-        message.channel.send(
-          "Enter your input within the next 10 seconds (Type `ex!` to exit): "
-        );
-        message.channel
-          .awaitMessages((m) => m.author.id == message.author.id, {
-            max: 1,
-            time: 10000,
-          })
-          .then((collected) => {
-            if (collected.first().content.toLowerCase() == "ex!")
-              message.channel.send(
-                "Operation Timeout by `" + message.author.username + "`"
-              );
-            else
-              message.channel.send(
-                JSCPP.run(code.join(" "), collected, config) + " " + output
-              );
-          })
-          .catch(() => {
-            message.reply("No answer after 10 seconds, operation canceled.");
-          });
-        var output = "";
-        var config = {
-          stdio: {
-            write: function (s) {
-              output += s;
-            },
-          },
-          unsigned_overflow: "error", // can be "error"(default), "warn" or "ignore"
-        };
-      }
-    }
+    var code = message.content.split(" ").slice(1);
+    message.channel.send(
+      "Enter your input within the next 10 seconds (Type `ex!` to exit): "
+    );
+    message.channel
+      .awaitMessages((m) => m.author.id == message.author.id, {
+        max: 1,
+        time: 10000,
+      })
+      .then((collected) => {
+        if (collected.first().content.toLowerCase() == "ex!")
+          message.channel.send(
+            "Operation Timeout by `" + message.author.username + "`"
+          );
+        else
+          message.channel.send(
+            JSCPP.run(code.join(" "), collected, config) + " " + output
+          );
+      })
+      .catch(() => {
+        message.reply("No answer after 10 seconds, operation canceled.");
+      });
+    var output = "";
+    var config = {
+      stdio: {
+        write: function (s) {
+          output += s;
+        },
+      },
+      unsigned_overflow: "error", // can be "error"(default), "warn" or "ignore"
+    };
   },
 };
