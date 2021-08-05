47a5b64291e90bfded085598cc39dc4090e73064
exoad <error666jm@gmail.com>
2021-08-05 10:04:36 -0400

    idk

---
+++ commands/global/code_test.js
@@ -0,0 +1,17 @@
+var JSCPP = require("JSCPP");
+
+module.exports = {
+  config: {
+    name: `code`,
+    category: "",
+    description: "",
+    aliases: [``],
+  },
+  run: async (bot, message, args) => {
+    try {
+      message.channel.send(message.content.split(" ").slice(1));
+    } catch (e) {
+      console.log(e);
+    }
+  },
+};
