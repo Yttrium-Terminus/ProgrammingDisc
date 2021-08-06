581cf68f919ba39a71cd59c76482f561793884ab
exoad <error666jm@gmail.com>
2021-08-06 10:36:46 -0400

    added separate commands for input and output

--- commands/global/code_test.js
+++ commands/global/code_test.js
@@ -1,94 +1,95 @@
 var compiler = require("compilex");
+const { MessageEmbed } = require("discord.js");
 var tio = require("tio.js");
 var options = { stats: true };
 compiler.init(options);
+var content = require('../../configs/content.json')
 
 module.exports = {
   config: {
     name: `cpp`,
     category: "",
     description: "",
-    aliases: [`c++`, `gcc`, `g++`, `cpluplus`],
+    aliases: [`c++`],
   },
   run: async (bot, message, args) => {
-    var code = message.content.split(" ").slice(1);
-    var linterX = { OS: "windows", cmd: "g++", options: { timeout: 20000 } };
-    message.reply(
-      "**Input?**\nType `yes` for input or `no` for no input and `exit` to exit **10 SECOND LIMIT**"
-    );
-    message.channel
-      .awaitMessages((m) => m.author.id == message.author.id, {
-        max: 1,
-        time: 10000,
-      })
-      .then((collected) => {
-        if (
-          collected.first().content.toLowerCase() == "no" ||
-          collected.first().content.toLowerCase() == "n" ||
-          collected.first().content.toLowerCase() == "NO" ||
-          collected.first().content.toLowerCase() == "No" ||
-          collected.first().content.toLowerCase() == "N"
-        ) {
-          message.channel.send("**NO INPUT**");
-          compiler.compileCPP(linterX, code.join(" "), function (data) {
-            if (data.error) {
-              message.channel.send(
-                "**An Error Occured!**\n```" + data.error + "```"
-              );
-              console.log(data.error);
-            } else {
-              console.log(data.output);
-              message.channel.send(data.output);
-            }
-          });
-        } else if (
-          collected.first().content.toLowerCase() == "exit" ||
-          collected.first().content.toLowerCase() == "quit" ||
-          collected.first().content.toLowerCase() == "terminate"
-        ) {
-          message.channel.send(
-            "Operation Terminated by " + message.author.username
-          );
-        } else if (
-          collected.first().content.toLowerCase() == "yes" ||
-          collected.first().content.toLowerCase() == "y" ||
-          collected.first().content.toLowerCase() == "Y" ||
-          collected.first().content.toLowerCase() == "YES" ||
-          collected.first().content.toLowerCase() == "Yes"
-        ) {
-          message.channel
-            .awaitMessages((m) => m.author.id == message.author.id, {
-              max: 1,
-              time: 10000,
-            })
-            .then((prt) => {
-              compiler.compileCPPWithInput(
-                linterX,
-                code.join(" "),
-                prt.toString(),
-                function (data) {
-                  if (data.error) {
-                    message.channel.send(
-                      "**An Error Occured!**\n```log\n" + data.error + "```"
-                    );
-                    console.log(data.error);
-                  } else {
-                    console.log(data.output);
-                    message.channel.send(
-                      JSON.parse(JSON.stringify(data.output))
-                    );
-                  }
-                }
-              );
-            })
-            .catch(() => {
-              message.reply("Operation Cancelled after 10 seconds");
-            });
-        }
-      })
-      .catch((err) => {
-        message.reply("Operation Cancelled automatically after 10 seconds");
-        console.error(err);
-      });
+    try {
+      var code = message.content.split(" ").slice(1);
+      if (
+        code == "INFO" ||
+        code == "help" ||
+        code == "info" ||
+        code == "HELP" ||
+        code == undefined ||
+        !code
+      ) {
+        const embed = new MessageEmbed()
+          .setTitle("C++ Runner withOUT Input")
+          .setDescription(
+            "This is a basic C++ interpreter that **does not take input**)"
+          )
+          .addField("Usage", "```" + content.prefix + "cpp [user_code_here]```")
+          .addField(
+            "user_code_here",
+            "You will input your C++ Code here or command parameters like this one (see [additional_usages])"
+          )
+          .addField(
+            "Example Usage",
+            "**COMMAND**\n```cpp\n$icpp #include <iostream>\nusing namespace std;\nint main() {\n int a;\ncin >> a;\n cout << a << endl;\n}```\n**OUTPUT**\n`Hello World!`"
+          )
+          .addField("Additional parameters", "None, this does not take inputs")
+          .addField(
+            "Constraints",
+            "Do to security reasons, the runtime constraint for non-input programs will be 10 seconds, if your program runs longer, it will be automatically killed"
+          )
+          .addField("[additional_usages]", "`help`")
+          .setFooter("Pre-build");
+        message.channel.send(embed);
+      } else {
+        var linterX = {
+          OS: "windows",
+          cmd: "g++",
+          options: { timeout: 10000 },
+        };
+        compiler.compileCPP(linterX, code.join(" "), function (data) {
+          if (data.error) {
+            const embed = new MessageEmbed()
+            .setTitle("C++ Program Runner Exception")
+            .setDescription("Your program had an error! *Not supposed to happen? Contact my developer: `ex-exoad#9292`")
+            .addField("Error", "```"+data.error+"```")
+            .setColor("RED")
+            message.reply(embed);
+            const embed2 = new MessageEmbed()
+            .setTitle(message.author.id)
+            .addField("Code", "```"+code.join(" ")+"```")
+            .addField("Server", message.guild.id)
+            .addField("Error", data.error)
+            .setColor("RED")
+            bot.channels.cache.get(content.cpp_log).send(embed2);
+            console.log(data.error);
+          } else {
+            console.log(data.output);
+            const embed = new MessageEmbed()
+            .setTitle("C++ Program Runner Results")
+            .setDescription("See anomalies in the output or its an incorrect output? Contact my developer: `ex-exoad#9292`")
+            .addField("Output", "```"+data.output+"```")
+            .addField("Tags", "`cpp`, `no_input`, `10s_constraint`")
+            .setFooter("Action submitted by " + message.author.username)
+            .setColor("GREEN")
+            
+            message.reply(embed);
+            const embed2 = new MessageEmbed()
+              .setTitle(message.author.id)
+              .addField("Code", "```" + code.join(" ") + "```")
+              .addField("Out", data.output)
+              .addField("server", message.guild.id)
+              .addField("channel", message.channel.id);
+            bot.channels.cache.get(content.cpp_log).send(embed2);
+          }
+        });
+      }
+    } catch (e) {
+      console.log(e);
+    }
   },
 };
---
+++ commands/global/list_commands.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  config: {
+    name: `help`,
+    category: "",
+    description: "",
+    aliases: [``],
+  },
+  run: async (bot, message, args) => {
+    try {
+      const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Commands List")
+      .setDescription("Here you will find all the commands.\n**Note:** Commands with the letter `i` infront followed by its programming lang means that command is used for whne programs need input")
+      .addField("C++", "```icpp, cpp```")
+      .addField("Others", "```ping, help```")
+      .setFooter("2021-2022")
+      message.channel.send(embed);
+    } catch (e) {
+      console.log(e);
+    }
+  },
+};
