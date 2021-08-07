c9747e4e2cc7e401d94363b92729cbb4436b5b57
exoad <error666jm@gmail.com>
2021-08-07 11:49:10 -0400

    versionr beta 1.0

--- commands/global/list_commands.js
+++ commands/global/list_commands.js
@@ -16,7 +16,7 @@ module.exports = {
           "Here you will find all the commands.\n**Note:** Commands with the letter `i` infront followed by its programming lang means that command is used for when programs need input.\n\nAll commands will have the `help` argument after the command to let you know more about the command"
         )
         .addField("C++", "```icpp, cpp```")
-        .addField("Others", "```ping, help, tos, editor, info, report```")
+        .addField("Global", "```ping, help, tos, editor, info, report, changelog```")
         .setFooter("2021-2022");
       message.channel.send(embed);
     } catch (e) {
