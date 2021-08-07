b23943c70ed1a36a7377b1556b847441224b098c
exoad <error666jm@gmail.com>
2021-08-06 21:11:33 -0400

    add command

--- commands/global/list_commands.js
+++ commands/global/list_commands.js
@@ -16,7 +16,7 @@ module.exports = {
           "Here you will find all the commands.\n**Note:** Commands with the letter `i` infront followed by its programming lang means that command is used for whne programs need input"
         )
         .addField("C++", "```icpp, cpp```")
-        .addField("Others", "```ping, help, tos, editor```")
+        .addField("Others", "```ping, help, tos, editor, info```")
         .setFooter("2021-2022");
       message.channel.send(embed);
     } catch (e) {
